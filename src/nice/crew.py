bd3aec8c3d8b1c874b8f40742c5167c2d63d3e26
ky3ow <vova2341591@gmail.com>
2025-12-07 20:33:25 +0000

    add writer

--- src/nice/crew.py
+++ src/nice/crew.py
@@ -16,13 +16,20 @@ class Nice():
         return Agent(
             config=self.agents_config['researcher'], # type: ignore[index]
             verbose=True,
-            tools=[SerperDevTool()]
+            tools=[SerperDevTool(n_results=3)]
         )
 
     @agent
     def reporting_analyst(self) -> Agent:
         return Agent(
             config=self.agents_config['reporting_analyst'], # type: ignore[index]
+            verbose=True
+        )
+
+    @agent
+    def writer(self) -> Agent:
+        return Agent(
+            config=self.agents_config['writer'], # type: ignore[index]
             verbose=True
         )
 
@@ -36,7 +43,14 @@ class Nice():
     def reporting_task(self) -> Task:
         return Task(
             config=self.tasks_config['reporting_task'], # type: ignore[index]
-            output_file='report.md'
+            output_file='research.md'
+        )
+
+    @task
+    def gist_task(self) -> Task:
+        return Task(
+            config=self.tasks_config['gist_task'], # type: ignore[index]
+            output_file='gist.md'
         )
 
     @crew
@@ -48,10 +62,6 @@ class Nice():
         return Crew(
             agents=self.agents, # Automatically created by the @agent decorator
             tasks=self.tasks, # Automatically created by the @task decorator
-            process=Process.hierarchical,
-            manager_agent=Agent(
-                config=self.agents_config['manager'], # type: ignore[index]
-                verbose=True
-            ),
+            process=Process.sequential,
             verbose=True,
         )
